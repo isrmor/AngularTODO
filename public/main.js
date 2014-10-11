2b876aed722b7085dd1028ac21a93a17e0bc5d72
isrmor <israel.moreno.91@gmail.com>
2014-10-11 15:15:08 +0200

    Primera app Angular - Lista TODO

---
+++ public/main.js
@@ -0,0 +1,40 @@
+var angularTodo = angular.module('angularTodo', []);
+
+function mainController($scope, $http) {
+	$scope.formData = {};
+
+	//Cuando se caerga la página, pide al API todos los TODOs
+	$http.get('/api/todos')
+		.success(function(data) {
+			$scope.todos = data;
+			console.log(data)
+		})
+		.error(function(data) {
+			console.log('Error: ' + data);
+		});
+
+	//Cuando se añade un nuevo TODO, manda el texto a la api
+	$scope.createTodo = function(){
+		$http.post('/api/todos', $scope.formData)
+			.success(function(data) {
+				$scope.formData = {};
+				$scope.todos = data;
+				console.log(data);
+			})
+			.error(function(data) {
+				console.log('Error: ' + data);
+			});
+	};
+
+	//Borra un TODO despues de checkearlo como acabado
+	$scope.deleteTodo = function(id) {
+		$http.delete('/api/todos/' + id)
+			.success(function(data) {
+				$scope.todos = data;
+				console.log(data);
+			})
+			.error(function(data) {
+				console.log('Error: ' + data);
+			});
+	};
+}
